f27dc0ea3a791aed0e8bf22ee947cdb485c2ed8e
Timur Khasanov <timur.khasanov@nordstrom.com>
2021-08-23 23:14:59 +0300

    zuul changed to spring cloud gateway, postman improvements

--- game-service/src/main/java/com/example/gameservice/GameServiceApplication.java
+++ game-service/src/main/java/com/example/gameservice/GameServiceApplication.java
@@ -3,12 +3,11 @@ package com.example.gameservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableTransactionManagement
 public class GameServiceApplication {
--- wallet-service/src/main/java/com/example/wallet/WalletServiceApplication.java
+++ wallet-service/src/main/java/com/example/wallet/WalletServiceApplication.java
@@ -3,11 +3,10 @@ package com.example.wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableTransactionManagement
 public class WalletServiceApplication {
 	public static void main(String[] args) {
